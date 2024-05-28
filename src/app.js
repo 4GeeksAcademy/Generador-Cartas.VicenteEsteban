e19831447b12e6fc0a03f899cc55a7d9d2268c16
Seeent <165791835+Seeent@users.noreply.github.com>
2024-05-28 15:17:51 +0000

    Generador cartas aleatorias

--- src/app.js
+++ src/app.js
@@ -7,5 +7,36 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(`.paloTop`).innerHTML = generateRandomTop();
+  document.querySelector(`.number`).innerHTML = generateRandomNumber();
+  document.querySelector(`.paloBot`).innerHTML = document.querySelector(
+    `.paloTop`
+  ).innerHTML;
 };
+let generateRandomNumber = () => {
+  let number = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indexNumber = Math.floor(Math.random() * number.length);
+  return number[indexNumber];
+};
+
+let generateRandomTop = () => {
+  let palo = ["♦", "♥", "♠", "♣"];
+  let indexTop = Math.floor(Math.random() * palo.length);
+  return palo[indexTop];
+};
+let palo = document.querySelector("#palo");
+palo.style.color = "red";
